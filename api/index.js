f27612aab35987054313ad1c4b32db10efab4a59
LadPaule <ssozipaule@gmail.com>
2022-01-15 20:52:59 +0300

    server rectification

--- api/index.js
+++ api/index.js
@@ -1,15 +1,15 @@
 const express = require("express");
 const cors = require("cors");
-const authRoutes = require("./routes/auth.js");
+const authRoutes = require("./auth.js");
 const app = express();
 const PORT = process.env.PORT || 5000;
 
 require("dotenv").config();
 app.options(
   "*",
-  cors({ origin: "https://scu.vercel.app/", optionsSuccessStatus: 200 })
+  cors({ origin: "https://scu.vercel.app/"||"https://www.smilecharityuganda.org/", optionsSuccessStatus: 200 })
 );
-app.use(cors({ origin: "https://scu.vercel.app/" , optionsSuccessStatus: 200 }));
+app.use(cors({ origin: "https://scu.vercel.app/" ||"https://www.smilecharityuganda.org/" , optionsSuccessStatus: 200 }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
